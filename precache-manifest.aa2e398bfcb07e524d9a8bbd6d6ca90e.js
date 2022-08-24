59f385ccd6f91b1c12a1c6149c65dd191e3bff85
Arush Sarkar <arushsarkar13021998@gmail.com>
2022-08-24 18:03:30 +0530

    Updates

---
+++ precache-manifest.aa2e398bfcb07e524d9a8bbd6d6ca90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dc4a86926af06cd075e",
+    "url": "/TextUtils/static/css/main.00eeca02.chunk.css"
+  },
+  {
+    "revision": "1dc4a86926af06cd075e",
+    "url": "/TextUtils/static/js/main.1dc4a869.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "5cc7339a8f010edd9075",
+    "url": "/TextUtils/static/js/2.5cc7339a.chunk.js"
+  },
+  {
+    "revision": "94fa866583836ad85178",
+    "url": "/TextUtils/static/js/runtime~main.94fa8665.js"
+  },
+  {
+    "revision": "a7720cb8f47781bf00e1287dcb880230",
+    "url": "/TextUtils/index.html"
+  }
+];
